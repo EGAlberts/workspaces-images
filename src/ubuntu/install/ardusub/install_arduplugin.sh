625beb0ec8535fdf2a2ac0a16fed3df00a774c14
EGAlberts <45923444+EGAlberts@users.noreply.github.com>
2023-01-16 11:39:40 +0100

    Hackathon install scripts

--- src/ubuntu/install/ardusub/install_arduplugin.sh
+++ src/ubuntu/install/ardusub/install_arduplugin.sh
@@ -3,11 +3,11 @@ set -ex
 
 sudo apt install -y rapidjson-dev libgz-sim7-dev
 cd $HOME
-git clone https://github.com/ArduPilot/ardupilot_gazebo -b ignition-garden
+git clone https://github.com/ArduPilot/ardupilot_gazebo
 cd ardupilot_gazebo
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -j4
 
-echo 'export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:${IGN_GAZEBO_SYSTEM_PLUGIN_PATH}' >> ~/.bashrc
-echo 'export IGN_GAZEBO_RESOURCE_PATH=$HOME/ardupilot_gazebo/models:$HOME/ardupilot_gazebo/worlds:${IGN_GAZEBO_RESOURCE_PATH}' >> ~/.bashrc
+echo 'export GZ_SIM_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:${GZ_SIM_SYSTEM_PLUGIN_PATH}' >> ~/.bashrc
+echo 'export GZ_SIM_RESOURCE_PATH=$HOME/ardupilot_gazebo/models:$HOME/ardupilot_gazebo/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
--- src/ubuntu/install/ardusub/install_ardusub.sh
+++ src/ubuntu/install/ardusub/install_ardusub.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -ex
-
-export USER=kasm-user
-
 cd $HOME
-git clone https://github.com/ArduPilot/ardupilot.git -b Sub-4.1 --recurse-submodules
-
+git clone https://github.com/ArduPilot/ardupilot.git --recurse-submodules
 
 cd ardupilot
+
+git checkout c623ae8
 
 #install-prereqs-ubuntu.sh from ardupilot
 
@@ -16,3 +14,10 @@ sudo apt install python-is-python3
 sudo pip3 install future
 
 sudo pip3 install -U mavproxy pymavlink
+
+export USER=kasm-user
+~/ardupilot/Tools/environment_install/install-prereqs-ubuntu.sh -y
+
+cd ~/ardupilot
+./waf configure
+./waf clean
