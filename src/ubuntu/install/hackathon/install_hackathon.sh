7b5cc00d40b6a8a0369f5ad824af03b515b3f793
Elvin <elvingalberts@gmail.com>
2023-01-18 22:42:50 +0100

    Added core image repo copy within, updated scripts

--- src/ubuntu/install/hackathon/install_hackathon.sh
+++ src/ubuntu/install/hackathon/install_hackathon.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 set -ex
 
-mkdir -p ~/tudelft_hackathon_ws/src
-cd ~/tudelft_hackathon_ws
+mkdir -p ~/pipeline_ws/src
+cd ~/pipeline_ws
 
-wget https://raw.githubusercontent.com/remaro-network/tudelft_hackathon/ros2/hackathon.rosinstall
-vcs import src < hackathon.rosinstall --recursive
+wget https://raw.githubusercontent.com/kas-lab/pipeline_inspection/main/pipeline_inspection/pipeline_inspection.rosinstall
+vcs import src < pipeline_inspection.rosinstall --recursive
 
-echo 'export GZ_SIM_RESOURCE_PATH=$HOME/tudelft_hackathon_ws/src/bluerov2_ignition/models:$HOME/tudelft_hackathon_ws/src/bluerov2_ignition/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
+echo 'export GZ_SIM_RESOURCE_PATH=$HOME/pipeline_ws/src/bluerov2_ignition/models:$HOME/pipeline_ws/src/bluerov2_ignition/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
 
-echo 'export GZ_SIM_RESOURCE_PATH=$HOME/tudelft_hackathon_ws/src/remaro_worlds/models:$HOME/tudelft_hackathon_ws/src/remaro_worlds/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
+echo 'export GZ_SIM_RESOURCE_PATH=$HOME/pipeline_ws/src/remaro_worlds/models:$HOME/pipeline_ws/src/remaro_worlds/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
 
 source /opt/ros/humble/setup.bash
-cd ~/tudelft_hackathon_ws/
+cd ~/pipeline_ws/
 
 sudo rosdep init
 rosdep update
 
 rosdep install --from-paths src --ignore-src -r -y
 
-cd ~/tudelft_hackathon_ws/
+cd ~/pipeline_ws/
 colcon build --symlink-install
 
 sudo apt-get install -y python3-dev python3-opencv python3-wxgtk4.0 python3-pip python3-matplotlib python3-lxml python3-pygame
 pip3 install PyYAML mavproxy --user
 echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 
-cd ~/tudelft_hackathon_ws/src/mavros/mavros/scripts
+cd ~/pipeline_ws/src/mavros/mavros/scripts
 
 sudo ./install_geographiclib_datasets.sh
 
